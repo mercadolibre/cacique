10e21b87906149e0484e6d2dd44c968655f325af
LulaBrizuela <lula.brizuela@gmail.com>
2011-01-10 17:28:14 -0300

    Added hoptoad key

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,4 +1,4 @@
 #suggestion
-#HoptoadNotifier.configure do |config|
-#  config.api_key = 'your_api_key'
-#end
+HoptoadNotifier.configure do |config|
+  config.api_key = '5f8f99cc9c55dfafa691063f2832e967'
+end
