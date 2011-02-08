b894303bb3ad3c79c65abe56878ab96c6960811b
Juan Luna <juan@juan-laptop.(none)>
2011-02-08 12:52:05 -0300

    Save button Show

--- public/javascripts/key_handler.js
+++ public/javascripts/key_handler.js
@@ -9,12 +9,8 @@ function restrictEnterKey(event) {
      if((key == 13) || (key == 8) || (key == 9) || ( (key >= 41) && (key <= 255) ) || (key == 32)){
         document.getElementById('modified_file').innerHTML = "<image src='/images/icons/s-edit.png'></img>";
     	document.getElementById('save_button').style.visibility = "visible"; 
-	document.getElementById('savewc_button').style.visibility = "visible"; 		
+	    document.getElementById('savewc_button').style.visibility = "visible"; 		
         add_asterisk = false;
      }
   }
 }
-
-
-
-
