3a3f6419ff20a9fd560605b1e77eca97737e0c72
Horacio Branciforte <horaciob@gmail.com>
2011-04-08 17:15:49 -0300

    creating inactive users by default

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,12 @@ describe User do
      @user = Factory(:user)
   end
 
-  it "should create a new instance given valid attributes" do
-    @user.save.should be_true
+  #it "should create a new instance given valid attributes" do
+  #  @user.save.should 0
+  #end
+  it "Users should be created as inactive" do
+
+    User.create(:login=>"testing1",:name=>"test",:email=>"testing@test.com",:password=>"test",:password_confirmation=>"test")
+    User.active?("testing1").should equal(0)
   end
 end
