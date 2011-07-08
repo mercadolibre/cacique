09d4be38e29effcf57268dd2bb9b16a34418e8c7
Horacio Branciforte <horaciob@gmail.com>
2011-07-08 15:07:26 -0300

    adding production config

--- script/memcached.sh
+++ script/memcached.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 case "$1" in
   start)
-	/usr/bin/memcached -P /tmp/memcached.pid -u cacique  -d -l 10.4.0.30
-	sleep 1
+        /usr/bin/memcached -P /tmp/memcached.pid -u cacique  -d -l 10.4.0.30
+        sleep 1
   ;;
   stop)
-	/bin/rm /tmp/memcached.pid
-	kill `pgrep memcached`
+        /bin/rm /tmp/memcached.pid
+        kill `pgrep memcached`
   ;;
 esac
