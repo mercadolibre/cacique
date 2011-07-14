58c4fd0b6bda1f2cea6c09bfb7dccb6532911a9d
BrendaGuerra <brenda.guerra.7@gmail.com>
2011-07-14 09:36:45 -0300

    upload files permit

--- app/controllers/data_files_controller.rb
+++ app/controllers/data_files_controller.rb
@@ -13,22 +13,25 @@ class DataFilesController < ApplicationController
 
 
   def create
-      upload = Hash.new
-      upload[:name]       = params['name']
-      upload[:project_id] = params['project_id']
-      upload[:fileUpload] = params['fileUpload'].read
-      upload[:file_name]  = params['fileUpload'].original_filename
-      begin
-         if DataFile.create( upload ) 
-           redirect_to url_for(:action=>:index, :project_id=>params['project_id'])
-         else
-            flash.now[:error] = _('There is already a file with that name')
-            render :action => 'new'
-         end
-      rescue Exception => e
-          flash.now[:error] =  e.message + "/n/n An error occurred, please check that SHARED_DIRECTORY is correctly defined in the file /config/cacique_conf"
-          render :action => 'new'        
-      end
+      @project = Project.find params['project_id']
+      permit "editor of :project" do
+        upload = Hash.new
+        upload[:name]       = params['name']
+        upload[:project_id] = params['project_id']
+        upload[:fileUpload] = params['fileUpload'].read
+        upload[:file_name]  = params['fileUpload'].original_filename
+        begin
+           if DataFile.create( upload ) 
+             redirect_to url_for(:action=>:index, :project_id=>params['project_id'])
+           else
+              flash.now[:error] = _('There is already a file with that name')
+              render :action => 'new'
+           end
+        rescue Exception => e
+            flash.now[:error] =  e.message + "/n/n An error occurred, please check that SHARED_DIRECTORY is correctly defined in the file /config/cacique_conf"
+            render :action => 'new'        
+        end
+     end
   end
 
 end
