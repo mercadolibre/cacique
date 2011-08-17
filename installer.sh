ce67eb832aa19cfa5d204d1ba82d4d342d5c0e99
Sebastian Otaegui <feniix@gmail.com>
2011-08-17 11:41:36 -0500

    changes to be able to run the install in a recently build ubuntu LTS

--- installer.sh
+++ installer.sh
@@ -3,26 +3,25 @@
  echo "===============================================" >> installer.log
  date>> installer.log
  echo "-----------------------------------------------" >> installer.log
- sudo apt-get install ruby1.8 2>> installer.log
- sudo apt-get install ruby1.8-dev  2>> installer.log
- sudo apt-get install rubygems   2>> installer.log
- sudo apt-get install g++   2>> installer.log
- sudo apt-get install libxslt1-dev  2>> installer.log
- sudo apt-get install libopenssl-ruby  2>> installer.log
- sudo apt-get install graphviz   2>> installer.log
- sudo apt-get install build-essential  2>> installer.log
- sudo apt-get install libmysql-ruby   2>> installer.log
- sudo apt-get install apache2 libcurl4-openssl-dev libssl-dev apache2-prefork-dev libapr1-dev libaprutil1-dev  2>> installer.log
- sudo apt-get install mysql-server mysql-server  2>> installer.log
- sudo apt-get install libmysqlclient15off libmysqlclient15-dev mysql-client mysql-common  2>> installer.log
- sudo apt-get install libc6-dev gcc libfcgi-dev ruby1.8-dev  2>> installer.log
- sudo apt-get install libfcgi-ruby1.8 libfcgi-dev  2>> installer.log
- sudo apt-get install catdoc  2>> installer.log
- sudo apt-get install curl  2>> installer.log
- sudo apt-get install memcached  2>> installer.log
- sudo gem install rubygems-update -v 1.3.6 2>> installer.log
+ sudo apt-get -y install git-core 2>> installer.log
+ sudo apt-get -y install ruby 2>> installer.log
+ sudo apt-get -y install ruby-dev 2>> installer.log
+ sudo apt-get -y install rubygems 2>> installer.log
+ sudo apt-get -y install build-essential 2>> installer.log
+ sudo apt-get -y install libxslt1-dev 2>> installer.log
+ sudo apt-get -y install libopenssl-ruby 2>> installer.log
+ sudo apt-get -y install graphviz 2>> installer.log
+ sudo apt-get -y install libmysql-ruby 2>> installer.log
+ sudo apt-get -y install apache2 libcurl4-openssl-dev libssl-dev apache2-prefork-dev libapr1-dev libaprutil1-dev 2>> installer.log
+ sudo apt-get -y install mysql-server 2>> installer.log
+ sudo apt-get -y install libmysqlclient-dev mysql-client 2>> installer.log
+ sudo apt-get -y install libfcgi-ruby1.8 libfcgi-dev 2>> installer.log
+ sudo apt-get -y install catdoc 2>> installer.log
+ sudo apt-get -y install curl 2>> installer.log
+ sudo apt-get -y install memcached 2>> installer.log
+ sudo gem install -V rubygems-update -v 1.3.6 2>> installer.log
  sudo /var/lib/gems/1.8/bin/./update_rubygems
- sudo gem install bundler 2>> installer.log
+ sudo gem install -V bundler 2>> installer.log
  bundle install --without development test
 echo ""
 echo ""
@@ -32,8 +31,9 @@ echo ""
 LINEAS=`cat /etc/profile | grep gems | wc -l`
 if [ $LINEAS = 0  ]
 then
-  var=`gem env gempath`
-  echo 'export PATH='${var//://bin:}'/bin:$PATH'  >> /etc/profile
+  echo "Adding export PATH=/var/lib/gems/1.8/bin:$PATH to the global profile file (/etc/profile)"
+  echo "so it exports the bin path for the global ruby gems"
+  echo "export PATH=/var/lib/gems/1.8/bin:\$PATH"  >> /etc/profile
   echo "Cacique will add rubygems path to your system,"
   echo "please reload your profile doing 'source /etc/profile' "
   echo ""
@@ -42,10 +42,11 @@ fi
 LINEAS=`grep '.gem/ruby/1.8/bin' $HOME/.bashrc | wc -l` 
 if [ $LINEAS = 0 ]
 then
-  PATH='$PATH':$HOME/.gem/ruby/1.8/bin >> $HOME/.bashrc
-  echo PATH='$PATH':$HOME/.gem/ruby/1.8/bin >> $HOME/.bashrc
+  echo "Adding export PATH=$HOME/.gem/ruby/1.8/bin:$PATH to the user bash resource file"
+  echo "so it handles the user ruby gems installation"
+  echo "export PATH=$HOME/.gem/ruby/1.8/bin:\$PATH" >> $HOME/.bashrc
+  echo "Please log out and re login or source your .bashrc to see the changes"
 fi
-sudo /bin/ln -s /usr/bin/ruby1.8 /usr/bin/ruby
 
 echo "-----------------------------------------------" >> installer.log
 echo "If you need more information, please contact us to robot@mercadolibre.com"
