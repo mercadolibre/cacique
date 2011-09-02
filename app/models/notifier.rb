f41a1c7f75e686ec45f964522c43e4f6fb560a89
Horacio Branciforte <horaciob@gmail.com>
2011-09-02 16:58:43 -0300

    adding SERVER_DOMAIN on mails and cacique_conf

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -40,7 +40,7 @@ class Notifier < ActionMailer::Base
    @from = EMAIL
    @subject = _("[Cacique] Password Recovery")
    @body['user'] = user
-   @body['url'] = "http://" + IP_SERVER.to_s + ":" + server_port.to_s + "/users/change_password/" + user.salt.to_s
+   @body['url'] = "http://" + SERVER_DOMAIN=.to_s + ":" + server_port.to_s + "/users/change_password/" + user.salt.to_s
    @content_type = "text/html"
   end
 
@@ -59,7 +59,7 @@ class Notifier < ActionMailer::Base
     @subject = "[CCQ] [#{suite_execution.s_status}] #{suite_execution.suite.name}"
     @body['suite_execution'] = suite_execution
     @attachment = {:content_type => "image/png",:filename=> "image.png", :body=> File.read(Rails.root.join('public/images/cacique/logo_cacique.png'))}
-    @url = "http://" + IP_SERVER.to_s + "/suite_executions/#{suite_execution.id}"  
+    @url = "http://" + SERVER_DOMAIN=.to_s + "/suite_executions/#{suite_execution.id}"  
     @content_type = "text/html"
   end 
   
@@ -69,7 +69,7 @@ class Notifier < ActionMailer::Base
       @from = EMAIL
       @subject = "[CCQ] [#{@execution.s_status}] #{@execution.circuit.name} "
       @body['execution'] = @execution
-      @url = "http://" + IP_SERVER.to_s + "/suite_executions/#{suite_execution.id}"      
+      @url = "http://" + SERVER_DOMAIN=.to_s + "/suite_executions/#{suite_execution.id}"      
       @content_type = "text/html"
   end
   
@@ -77,7 +77,7 @@ class Notifier < ActionMailer::Base
     task_program = TaskProgram.find task_program_id
     @recipients = user_mail
     @from = EMAIL
-    @body['url_confirm'] = "http://" + IP_SERVER.to_s + ":" + server_port.to_s + "/task_programs/confirm_program/#{task_program_id}"
+    @body['url_confirm'] = "http://" + SERVER_DOMAIN=.to_s + ":" + server_port.to_s + "/task_programs/confirm_program/#{task_program_id}"
     @body['suite_name'] = Suite.find(suite_id).name
     @body['next_execution'] = task_program.delayed_jobs[1] 
     @content_type = "text/html" 
