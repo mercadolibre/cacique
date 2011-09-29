519025577cecedd3eb140bfc339d67fa26a2dd63
BrendaGuerra <brenda.guerra.7@gmail.com>
2011-09-29 14:37:59 -0300

    images titles in user functions show

--- app/controllers/user_functions_controller.rb
+++ app/controllers/user_functions_controller.rb
@@ -32,9 +32,6 @@ class UserFunctionsController < ApplicationController
     @project_id   = params[:filter] ? params[:filter][:project_id].to_s : ""
     params[:text] = params[:filter][:text]  if( params[:filter] and params[:filter][:text] ) #Search
     @public = params[:visibility] = true if( !params[:filter] or (params[:filter] and params[:filter][:project_id] == "")) #Public functions
-
-p 'aaaaaaa'
-p params
     @search = UserFunction.get_user_functions_with_filters([@project_id], params)   
     @user_functions = @search.paginate :page => params[:page], :per_page => 20
     @param_search   = ( !params[:filter].nil? ?  params[:filter][:text] : nil )
