4652c7e5cb2f9494ae94dd4a6b953e40f447bcfb
BrendaGuerra <brenda.guerra.7@gmail.com>
2011-09-29 15:19:05 -0300

    edit and function created private checlbox

--- app/controllers/user_functions_controller.rb
+++ app/controllers/user_functions_controller.rb
@@ -156,7 +156,7 @@ class UserFunctionsController < ApplicationController
       @user_function.description = params[:user_function][:description].to_s
       @user_function.cant_args = args.length
       @user_function.example = params[:user_function][:example]
-      @user_function.visibility = (params[:user_function][:visibility] == "1" ? true : false)
+      @user_function.visibility = ( params[:user_function][:visibility] == "1" ) 
       @user_function.hide = (params[:user_function][:hide] == "1" ? true : false)
       #source_code Generate
       code=params[:user_function][:code].split("_")[1..-1].map{|x| decode_char(x) }.join
