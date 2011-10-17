7b66b772055820a487b62ef797c21b7964e9b19f
Horacio <horaciob@gmail.com>
2011-10-17 11:46:53 -0300

    adding conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 
 # Use a different cache store in production
- config.cache_store = :mem_cache_store,10.4.0.30
+ config.cache_store = :mem_cache_store,"127.0.0.1"
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
