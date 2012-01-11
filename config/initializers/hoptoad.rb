233e4e0472eff646149d4f4c293b7fca40de2e71
Horacio Branciforte <horacio@gmail.com>
2012-01-11 15:34:22 -0300

    deleting hopptoad

--- config/initializers/hoptoad.rb
+++
@@ -1,8 +0,0 @@
-#suggestion
-#HoptoadNotifier.configure do |config|
-#  config.api_key = '5f8f99cc9c55dfafa691063f2832e967'
-#end
-#HoptoadNotifier.configure do |config|
-#  config.api_key = '85f4c74fa1bfacae5e9995c793b0777d'
-#end
-
