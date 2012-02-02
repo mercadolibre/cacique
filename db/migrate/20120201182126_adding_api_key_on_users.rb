d7867ab35721014672cceb1b05559dc9e4486663
Horacio Branciforte <horacio@gmail.com>
2012-02-02 09:41:05 -0300

    refreshin model

--- db/migrate/20120201182126_adding_api_key_on_users.rb
+++ db/migrate/20120201182126_adding_api_key_on_users.rb
@@ -1,5 +1,6 @@
 class AddingApiKeyOnUsers < ActiveRecord::Migration
   def self.up
+     User.reset_column_information
      User.find_all_by_api_key("").each do |usr|
         usr.enable_api!
      end
